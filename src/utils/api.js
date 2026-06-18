b03783df73a9ee32ebec1155adf2b2c07cc88646
sujeet546 <sujeetchaudhary8750@gmail.com>
2026-06-18 13:26:57 +0530

    api change

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://multishop-backend-0ifz.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL ,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
